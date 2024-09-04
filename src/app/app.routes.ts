f0062facbc7a4434591a3e3e7f58065c62221d88
cacalo <cacalo219@hotmail.com>
2024-09-04 17:16:31 -0300

    Pagina login sin funcionar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { EstadoCocherasComponent } from './pages/estado-cocheras/estado-cocheras.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "estado-cocheras",
+        component: EstadoCocherasComponent
+    },
+    {
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
+    },
+    {
+        path: "not-found",
+        component: NotFoundComponent
+    },
+    {
+        path: "**",
+        redirectTo: "not-found",
+        pathMatch: "full"
+    },
+];
---
+++ src/app/pages/estado-cocheras/estado-cocheras.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-estado-cocheras',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './estado-cocheras.component.html',
+  styleUrl: './estado-cocheras.component.scss'
+})
+export class EstadoCocherasComponent {
+
+}
